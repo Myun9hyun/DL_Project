6e6b7dbc41e85d82851d8725b9b04e4dca55b93b
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-11 15:27:43 +0900

    dd

--- MH/app.py
+++ MH/app.py
@@ -40,27 +40,27 @@ file_id = '1kLo4A1qbyn1D2aMRwkpLPp1ehHe1eVz3'
 destination = 'Myun9hyun/DL_Project/MH/vgg_weights_1000.pth'
 download_file_from_google_drive(file_id, destination)
 # 모델 불러오기
-model = torch.load(destination)
-# 스트림릿 앱 구현
-st.title("딥러닝 모델 구현")
-# 이미지 업로드
-uploaded_file = st.file_uploader("이미지 업로드", type=["png", "jpg", "jpeg"])
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption='업로드한 이미지', use_column_width=True)
-    # 이미지 전처리
-    transform = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        transforms.Normalize(
-            mean=[0.485, 0.456, 0.406],
-            std=[0.229, 0.224, 0.225]
-        )
-    ])
-    input_tensor = transform(image).unsqueeze(0)
-    # 모델 예측
-    with torch.no_grad():
-        model.eval()
-        output = model(input_tensor)
-        prediction = torch.argmax(output, dim=1).item()
-    st.write("예측 결과:", prediction)
+# model = torch.load(destination)
+# # 스트림릿 앱 구현
+# st.title("딥러닝 모델 구현")
+# # 이미지 업로드
+# uploaded_file = st.file_uploader("이미지 업로드", type=["png", "jpg", "jpeg"])
+# if uploaded_file is not None:
+#     image = Image.open(uploaded_file)
+#     st.image(image, caption='업로드한 이미지', use_column_width=True)
+#     # 이미지 전처리
+#     transform = transforms.Compose([
+#         transforms.Resize((224, 224)),
+#         transforms.ToTensor(),
+#         transforms.Normalize(
+#             mean=[0.485, 0.456, 0.406],
+#             std=[0.229, 0.224, 0.225]
+#         )
+#     ])
+#     input_tensor = transform(image).unsqueeze(0)
+#     # 모델 예측
+#     with torch.no_grad():
+#         model.eval()
+#         output = model(input_tensor)
+#         prediction = torch.argmax(output, dim=1).item()
+#     st.write("예측 결과:", prediction)
