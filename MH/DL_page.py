5ed7d138bacd038cb28123bb9f774dafd103987c
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-11 13:45:18 +0900

    dd

--- MH/DL_page.py
+++ MH/DL_page.py
@@ -202,7 +202,8 @@ if choice == "페이지1":
         # Define a function to visualize the computation graph
         def visualize_graph():
             # Load the pre-trained VGG19 model
-            model = vgg16(pretrained=True)
+
+            model = torchvision.models.vgg16(weights='imagenet')
             model = torch.load("MH/model/vgg_weights.pth", map_location=device)
 
             # Create a random input tensor
