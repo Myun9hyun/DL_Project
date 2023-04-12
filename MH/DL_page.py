842a15c7d02ecb2185dd065515a608cf4383737d
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-04-12 11:28:44 +0900

    colab링크 추가

--- MH/DL_page.py
+++ MH/DL_page.py
@@ -168,9 +168,10 @@ if choice == "페이지1":
         tab2.subheader("탭2")
         st.write()
         '''
-        ### 탭2
+        ### 다음은 딥러닝 실제 구현할 수 있는 Colab 링크입니다.
         '''
         
+        ![https://img.shields.io/badge/Colab-F9AB00?style=for-the-badge&logo=googlecolab&color=525252](https://colab.research.google.com/drive/1pWstcDuEqPFxV_ZLhNyQtzI9MRanz7Pv?usp=sharing)
 
 
     with tab3:
